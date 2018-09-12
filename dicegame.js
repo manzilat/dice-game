871d052fc2796a9a1671b20724c78c11f49204a0
manzilat <amtulnoorq@yahoo.com>
2018-09-12 16:20:12 -0500

    attached a bg picture and added some ifelse statements and also made some changes to select a die

--- dicegame.js
+++ dicegame.js
@@ -1,4 +1,5 @@
-
+let playerOneScore = 0;
+let playerTwoScore = 0;
 
 function rollDice(){
  
@@ -7,15 +8,34 @@ function rollDice(){
     var die3 = document.getElementById("die3");
     var die4 = document.getElementById("die4");
     var die5 = document.getElementById("die5");
-    var die6 = document.getElementById("die6");
+    var die6 = document.getElementById("die6").innerHTML;
+
     var status = document.getElementById("status");
-    var d1 = Math.floor(Math.random() * 6) + 1;
-    var d2 = Math.floor(Math.random() * 6) + 1;
-    var d3 = Math.floor(Math.random() * 6) + 1;
-    var d4 = Math.floor(Math.random() * 6) + 1;
-    var d5 = Math.floor(Math.random() * 6) + 1;
-    var d6 = Math.floor(Math.random() * 6) + 1;
-    var diceTotal = d1 + d2 + d3 + d4 + d5 + d6;
+
+    // var d1 = Math.floor(Math.random() * 4) + 1;
+    // var d2 = Math.floor(Math.random() * 6) + 1;
+    // var d3 = Math.floor(Math.random() * 8) + 1;
+    // var d4 = Math.floor(Math.random() * 10) + 1;
+    // var d5 = Math.floor(Math.random() * 12) + 1;
+    // var d6 = Math.floor(Math.random() * 20) + 1;
+    // var diceTotal = d1 + d2 + d3 + d4 + d5 + d6;
+
+    let dropdownValue = document.getElementById("rollDice").value;
+    let diceArray = [4, 6, 8, 10, 12, 20];
+    let diceTotal = 0;
+    let rolls = [];
+
+    for(let i = 0; i < diceArray.length; i++ ) {
+        if(/*should still roll*/) { // compare 'i' to 'dropdownValue'
+            let roll = Math.floor(Math.random() * diceArray[i]) + 1;
+            diceTotal += roll;
+            rolls.push(roll);
+        }
+        else {
+            // set die to 0
+        }
+    }
+
     die1.innerHTML = d1;
     die2.innerHTML = d2;
     die3.innerHTML = d3;
@@ -27,5 +47,6 @@ function rollDice(){
         status.innerHTML += " DOUBLES! You get a free turn!!";
     }
 }
+
 
 
