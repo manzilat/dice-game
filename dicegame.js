8f62eae0ef68dcf3d81dfca84b9e78e8f53344c9
manzilat <amtulnoorq@yahoo.com>
2018-09-12 09:59:35 -0500

    trying to get number of dice on each selected option

--- dicegame.js
+++ dicegame.js
@@ -1,7 +1,45 @@
 
-
 function rollDice(){
- 
+ if (select = ONE){
+    var die1 = document.getElementById("die1"); 
+    var status = document.getElementById("status");
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    var diceTotal = d1;die1.innerHTML = d1;
+    status.innerHTML = "You rolled "+diceTotal+".";
+ }
+ else if(select =TWO){
+    var die1 = document.getElementById("die1");
+    var die2 = document.getElementById("die2");
+    var status = document.getElementById("status");
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    var d2 = Math.floor(Math.random() * 6) + 1;
+    var diceTotal = d1 + d2;
+    die1.innerHTML = d1;
+    die2.innerHTML = d2;
+    status.innerHTML = "You rolled "+diceTotal+".";
+    if(d1 == d2 ){
+        status.innerHTML += "  You get a free turn!!";
+ }
+}
+else if(select =THREE){
+    var die1 = document.getElementById("die1");
+    var die2 = document.getElementById("die2");
+    var die3 = document.getElementById("die3");
+    var status = document.getElementById("status");
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    var d2 = Math.floor(Math.random() * 6) + 1;
+    var d3 = Math.floor(Math.random() * 6) + 1;
+    var diceTotal = d1 + d2 + d3;
+    die1.innerHTML = d1;
+    die2.innerHTML = d2;
+    die3.innerHTML = d3;
+    status.innerHTML = "You rolled "+diceTotal+".";
+    if(d1 == d2 == d3 == d4 == d5 == d6){
+        status.innerHTML += "  You get a free turn!!";
+    }
+}
+else if (select=SIX){
+    
     var die1 = document.getElementById("die1");
     var die2 = document.getElementById("die2");
     var die3 = document.getElementById("die3");
@@ -24,6 +62,7 @@ function rollDice(){
     die6.innerHTML = d6;
     status.innerHTML = "You rolled "+diceTotal+".";
     if(d1 == d2 == d3 == d4 == d5 == d6){
-        status.innerHTML += " DOUBLES! You get a free turn!!";
+        status.innerHTML += " You get a free turn!!";
     }
+  }
 }
