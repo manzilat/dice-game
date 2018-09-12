9697117765e9d0b8445780f1fbafccd4e198964b
manzilat <amtulnoorq@yahoo.com>
2018-09-12 13:25:33 -0500

    fixed some errors that occured due to body tag

--- dicegame.js
+++ dicegame.js
@@ -1,92 +1,7 @@
 
-function rollDice(){
- if (select = ONE){
-    var die1 = document.getElementById("die1"); 
-    var status = document.getElementById("status");
-    var d1 = Math.floor(Math.random() * 6) + 1;
-    var diceTotal = d1;die1.innerHTML = d1;
-    status.innerHTML = "You rolled "+diceTotal+".";
- }
- else if(select =TWO){
-    var die1 = document.getElementById("die1");
-    var die2 = document.getElementById("die2");
-    var status = document.getElementById("status");
-    var d1 = Math.floor(Math.random() * 6) + 1;
-    var d2 = Math.floor(Math.random() * 6) + 1;
-    var diceTotal = d1 + d2;
-    die1.innerHTML = d1;
-    die2.innerHTML = d2;
-    status.innerHTML = "You rolled "+diceTotal+".";
-    if(d1 == d2 ){
-        status.innerHTML += "  You get a free turn!!";
- }
-}
-else if(select =THREE){
-    var die1 = document.getElementById("die1");
-    var die2 = document.getElementById("die2");
-    var die3 = document.getElementById("die3");
-    var status = document.getElementById("status");
-    var d1 = Math.floor(Math.random() * 6) + 1;
-    var d2 = Math.floor(Math.random() * 6) + 1;
-    var d3 = Math.floor(Math.random() * 6) + 1;
-    var diceTotal = d1 + d2 + d3;
-    die1.innerHTML = d1;
-    die2.innerHTML = d2;
-    die3.innerHTML = d3;
-    status.innerHTML = "You rolled "+diceTotal+".";
-    if(d1 == d2 == d3 == d4 == d5 == d6){
-        status.innerHTML += "  You get a free turn!!";
-    }
-}
-else if(select = FOUR )
-{
-    var die1 = document.getElementById("die1");
-    var die2 = document.getElementById("die2");
-    var die3 = document.getElementById("die3");
-    var die4 = document.getElementById("die4");
-    var status = document.getElementById("status");
-    var d1 = Math.floor(Math.random() * 6) + 1;
-    var d2 = Math.floor(Math.random() * 6) + 1;
-    var d3 = Math.floor(Math.random() * 6) + 1;
-    var d4 = Math.floor(Math.random() * 6) + 1;  
-    var diceTotal = d1 + d2 + d3 + d4;
-    die1.innerHTML = d1;
-    die2.innerHTML = d2;
-    die3.innerHTML = d3;
-    die4.innerHTML = d4; 
-    status.innerHTML = "You rolled "+diceTotal+".";
-    if(d1 == d2 == d3 == d4 ){
-        status.innerHTML += " You get a free turn!!";
-    
-  }
-}
-else if(select =FIVE){
-    var die1 = document.getElementById("die1");
-    var die2 = document.getElementById("die2");
-    var die3 = document.getElementById("die3");
-    var die4 = document.getElementById("die4");
-    var die5 = document.getElementById("die5");  
-    var status = document.getElementById("status");
-    var d1 = Math.floor(Math.random() * 6) + 1;
-    var d2 = Math.floor(Math.random() * 6) + 1;
-    var d3 = Math.floor(Math.random() * 6) + 1;
-    var d4 = Math.floor(Math.random() * 6) + 1;
-    var d5 = Math.floor(Math.random() * 6) + 1;  
-    var diceTotal = d1 + d2 + d3 + d4 + d5;
-    die1.innerHTML = d1;
-    die2.innerHTML = d2;
-    die3.innerHTML = d3;
-    die4.innerHTML = d4;
-    die5.innerHTML = d5;
-    
-    status.innerHTML = "You rolled "+diceTotal+".";
-    if(d1 == d2 == d3 == d4 == d5 ){
-        status.innerHTML += " You get a free turn!!";
-    }
-  }
 
-else if (select=SIX){
-    
+function rollDice(){
+ 
     var die1 = document.getElementById("die1");
     var die2 = document.getElementById("die2");
     var die3 = document.getElementById("die3");
@@ -109,8 +24,8 @@ else if (select=SIX){
     die6.innerHTML = d6;
     status.innerHTML = "You rolled "+diceTotal+".";
     if(d1 == d2 == d3 == d4 == d5 == d6){
-        status.innerHTML += " You get a free turn!!";
+        status.innerHTML += " DOUBLES! You get a free turn!!";
     }
-  }
 }
+
 
